7e5cd91d3683d98f79ada1e1ed4a01cc8c47e519
yuripassuelo <49758507+yuripassuelo@users.noreply.github.com>
2026-03-03 18:01:22 -0300

    nova_ver_pdf

--- src/01.Modelos_Probit.R
+++ src/01.Modelos_Probit.R
@@ -120,3 +120,61 @@ stargazer(mod_probit_2_dt, mod_probit_3_dt )
 
 stargazer( mod_probit_1_v2, mod_probit_2_v2, mod_probit_3_v2, mod_probit_1_dt_v2)
 stargazer( mod_probit_2_dt_v2, mod_probit_3_dt_v2 )
+
+
+# Efeitos Marginais Probit
+
+mfxboot <- function(modform,dist,data,boot=1000,digits=3){
+  x <- glm(modform, family=binomial(link=dist),data)
+  # get marginal effects
+  pdf <- ifelse(dist=="probit",
+                mean(dnorm(predict(x, type = "link"))),
+                mean(dlogis(predict(x, type = "link"))))
+  marginal.effects <- pdf*coef(x)
+  # start bootstrap
+  bootvals <- matrix(rep(NA,boot*length(coef(x))), nrow=boot)
+  set.seed(1111)
+  for(i in 1:boot){
+    samp1 <- data[sample(1:dim(data)[1],replace=T,dim(data)[1]),]
+    x1 <- glm(modform, family=binomial(link=dist),samp1)
+    pdf1 <- ifelse(dist=="probit",
+                   mean(dnorm(predict(x, type = "link"))),
+                   mean(dlogis(predict(x, type = "link"))))
+    bootvals[i,] <- pdf1*coef(x1)
+  }
+  res <- cbind(marginal.effects,apply(bootvals,2,sd),marginal.effects/apply(bootvals,2,sd))
+  if(names(x$coefficients[1])=="(Intercept)"){
+    res1 <- res[2:nrow(res),]
+    res2 <- matrix(as.numeric(sprintf(paste("%.",paste(digits,"f",sep=""),sep=""),res1)),nrow=dim(res1)[1])     
+    rownames(res2) <- rownames(res1)
+  } else {
+    res2 <- matrix(as.numeric(sprintf(paste("%.",paste(digits,"f",sep=""),sep="")),nrow=dim(res)[1]))
+    rownames(res2) <- rownames(res)
+  }
+  colnames(res2) <- c("marginal.effect","standard.error","z.ratio")  
+  return(res2)
+}
+
+mg_effct_mod_3    <- mfxboot( modform = dummy_conc_12 ~ d_pub_fed + d_pub_est + d_munic + p_sfl + comum + norte + nordeste + sul + coeste + idade_curso + ate_1_5_sm + de_15_a_3 + de_3_a_45 + de_45_a_6 + de_6_a_10 + perc_fem + idade_med + perc_n_trab + ate_ef_mae + es_com_mae + ate_ef_pai + es_com_pai + satisf_cur+ satisf_ies + nota_fies + nota_sisu,
+                              dist    = "probit",
+                              data    = df_final,
+                              boot    = 1000,
+                              digits  = 3)
+mg_effct_mod_3    <- as.data.frame( mg_effct_mod_3 )
+mg_effct_mod_3$V1 <- row.names( mg_effct_mod_3 )
+mg_effct_mod_3$me <- as.numeric(as.character(mg_effct_mod_3$marginal.effect))
+mg_effct_mod_3$se <- as.numeric(as.character(mg_effct_mod_3$standard.error))
+
+
+ggplot(mg_effct_mod_3, aes(V1, marginal.effect,ymin = me - 2*se,ymax= me + 2*se)) +
+  scale_x_discrete('Variable') +
+  #scale_y_continuous('Marginal Effect',limits=c(-0.5,1)) +
+  theme_bw() + 
+  geom_errorbar(aes(x = V1, y = me),linewidth=.3,width=.2) + 
+  geom_point(aes(x = V1, y = me)) +
+  geom_hline(yintercept=0) + 
+  coord_flip() 
+
+dummy_conc_12 ~ d_pub_fed + d_pub_est + d_munic + p_sfl + comum + norte + nordeste + sul + coeste + d_10_ys + d_20_ys + ate_1_5_sm + de_15_a_3 + de_3_a_45 + de_45_a_6 + de_6_a_10 + perc_fem + idade_med + perc_n_trab + ate_ef_mae + es_com_mae + ate_ef_pai + es_com_pai + satisf_cur+ satisf_ies + nota_fies + nota_sisu,
+mg_effct_mod_3_dt <-
+
